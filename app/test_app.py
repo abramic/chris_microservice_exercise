3678a930625709da7ac7e3cd9f7d9574c48221be
Chris Abrami <57453141+abramic@users.noreply.github.com>
2019-11-21 15:14:15 -0800

    Ran a few more tests - figured out how to run automated tests on the server, including tests with async calls to the db

--- app/test_app.py
+++ app/test_app.py
@@ -1,8 +1,15 @@
 from app import app
 import unittest
 from json import loads
+
+# The method definition MUST have test in the name, in order for the testing suite to run it as a test
+
 # To run this, run the following command (inside the app folder):  
 # python3 -m unittest test_app.py
+
+# The server should start up, and it'll just look like nothing's hitting it.  Let it run for 
+# a time and then stop it with Control+C.  The results of only the non passing tests should
+# show up
 
 # Also, in the flask app, just have app.run() with no port or debug options
 
@@ -25,12 +32,15 @@ class TestMyApp(unittest.TestCase):
        
         self.assertEqual(loads(rv.data), {"prop": "Hello World!"})
 
-    def test_three(self):
-        rv = self.app.get('/')
-        self.assertEqual(loads(rv.data), {"prop": "Hello Globe!"})
+    # def test_three(self):
+    #     rv = self.app.get('/')
+    #     self.assertEqual(loads(rv.data), {"prop": "Hello Globe!"})
 
-
-
+    def test_four(self):
+        rv = self.app.get('/locations?user_id=5dd46e6ec2dddbebad4078b6')
+        body = loads(rv.data)
+        print('THE BODY IS: ', body)
+        self.assertEqual(rv.status, '404 OK')
 
 if __name__ == "__main__":
     unittest.main()
